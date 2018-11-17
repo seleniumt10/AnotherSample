3761c054c9972da32d51633a7e128e992fcd1a4f
sukanya p <seleniumt10@gmail.com>
2018-11-17 10:52:02 +0530

    sukanya

--- src/main/java/selenium/October13/App.java
+++ src/main/java/selenium/October13/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hi Sukanya It's me Hansul" );
-        System.out.println("Hello Hello world");
+        System.out.println("Hello world world Hello");
    
     }
 }
